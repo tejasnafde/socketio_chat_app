52557f7e14069a401db96f29da370b3ef3de8c77
tejas44 <tejas@geoiq.io>
2025-03-09 15:41:46 +0530

    Added table creation sql commands to start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,11 @@
    #!/bin/bash
+   TABLE_EXISTS=$(psql $DATABASE_URL -t -c "SELECT to_regclass('gra_action_hub');")
+
+    if [ "$TABLE_EXISTS" = "" ]; then
+        echo "Table 'gra_action_hub' does not exist. Running migrations..."
+        flask db upgrade  # Run migrations to create the necessary tables
+    else
+        echo "Table 'gra_action_hub' already exists. Skipping migrations."
+    fi
+    
    gunicorn app:app --bind 0.0.0.0:$PORT
