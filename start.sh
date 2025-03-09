ecbe0cb4dda1109c42dffff49c04497dd03f9d39
tejas44 <tejas@geoiq.io>
2025-03-09 15:47:56 +0530

    Update start.sh to include database migrations

--- start.sh
+++ start.sh
@@ -1,11 +1,14 @@
-   #!/bin/bash
-   TABLE_EXISTS=$(psql $DATABASE_URL -t -c "SELECT to_regclass('gra_action_hub');")
+#!/bin/bash
 
-    if [ "$TABLE_EXISTS" = "" ]; then
-        echo "Table 'gra_action_hub' does not exist. Running migrations..."
-        flask db upgrade  # Run migrations to create the necessary tables
-    else
-        echo "Table 'gra_action_hub' already exists. Skipping migrations."
-    fi
-    
-   gunicorn app:app --bind 0.0.0.0:$PORT
+# Check if the table 'gra_action_hub' exists
+TABLE_EXISTS=$(psql $DATABASE_URL -t -c "SELECT to_regclass('gra_action_hub');")
+
+if [ "$TABLE_EXISTS" = "" ]; then
+    echo "Table 'gra_action_hub' does not exist. Running migrations..."
+    flask db upgrade  # Run migrations to create the necessary tables
+else
+    echo "Table 'gra_action_hub' already exists. Skipping migrations."
+fi
+
+# Start the Gunicorn server
+gunicorn app:app --bind 0.0.0.0:$PORT
